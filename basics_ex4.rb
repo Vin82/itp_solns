8183c2086e0ec2148e235f70f234b05e16e1016e
Vincenzo Altieri <vin.altieri@gmail.com>
2016-07-23 16:29:21 -0400

    Solutions for all exercises in ITP basics chapter

---
+++ basics_ex4.rb
@@ -0,0 +1,6 @@
+my_array = [1999, 1999, 1994, 1994, 2010]
+my_array[0]
+my_array[1]
+my_array[2]
+my_array[3]
+my_array[4]
---
+++ basics_ex7.rb
@@ -0,0 +1,1 @@
+# There was a right paren at the end of an expression and ruby was expecting a right bracket. Possibly when creating a dictionary.
