6e03c7b68b1c9a43497b888b3a17029d3f4a68ed
Vincenzo Altieri <vin.altieri@gmail.com>
2016-11-26 21:19:42 -0500

    ITP Hashes Chapter Solutions

---
+++ hashes_ex5.rb
@@ -0,0 +1,6 @@
+# The has_value? function.
+
+person = {name: 'Bob', occupation: 'web developer', hobbies: 'painting'}
+
+puts person.has_value?('Bob')
+puts person.has_value?('Mike')
---
+++ hashes_ex7.rb
@@ -0,0 +1,6 @@
+# In the first hash, the key is the letter x, which is assigned the value "some value"
+# In the second hash, the key is actually the string "hi there"
+x = "hi there"
+puts my_hash = {x: "some value"}
+puts my_hash2 = {x => "some value"}
+
